7d16d89560be5098d7a070a26efa94b7251ea95b
michalbieniek02 <michalbieniek02@gmail.com>
2023-12-11 10:50:03 +0100

    added logic scheme to LOGIC project

--- CheckedAppProject.LOGIC/Services/ItemService.cs
+++ CheckedAppProject.LOGIC/Services/ItemService.cs
@@ -9,8 +9,9 @@ namespace CheckedAppProject.LOGIC.Services
     public class ItemService
     {
         private void EditName(string itemName) { }
-        private void DeleteItem(string itemName,int itemListId) { }
+        private void DeleteItem(string itemName, int itemListId) { }
         private void ToggleItemToBuy(string itemName) { }
         private void ToggleItemToPack(string itemName) { }
 
     }
+}
