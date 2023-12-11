95255f739891efa6c52640fa1d1ade886462ccba
michalbieniek02 <michalbieniek02@gmail.com>
2023-12-11 10:38:54 +0100

    filled up logic project

---
+++ CheckedAppProject.LOGIC/DTOs/UserLoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckedAppProject.LOGIC.DTOs
+{
+    public class UserLoginDTO
+    {
+        public string UserPassword { get; set; }
+        public string UserEmail { get; set; }
+        public bool IsUserLogged { get; set; }
+    }
+}
---
+++ CheckedAppProject.LOGIC/Services/ItemListService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckedAppProject.LOGIC.Services
+{
+    public class ItemListService
+    {
+        public List<Item> ShowItemList(int id)
+        {
+
+        }
+        public void EditItemList(int id) { }
+        public List<Item> GetOneListByName(int id) { }
+        public List<ItemList> GetListsByTravelDestination(string destination) { }
+        public void AddFoundList(int id) { }
+        public string GetMonthName(DateTime time) 
+        {
+            string monthName = time.ToString("MMMM");
+            return monthName;
+        }
+    }
+}
