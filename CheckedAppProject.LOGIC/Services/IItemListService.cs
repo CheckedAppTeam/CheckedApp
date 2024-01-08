d0fca674875ce205f0298338fb3de22c1ee88f4a
“Anna <annazieli22@gmail.com>
2024-01-08 12:30:34 +0100

    findbycityandmonth and copy item list is provided to item list repository and they are working

--- CheckedAppProject.LOGIC/Services/IItemListService.cs
+++ CheckedAppProject.LOGIC/Services/IItemListService.cs
@@ -5,13 +5,14 @@ namespace CheckedAppProject.LOGIC.Services
 {
     public interface IItemListService
     {
+        Task<ItemList> CopyAsync(int itemListid, int userId);
         Task CreateAsync(CreateItemListDTO dto, int userId);
-        Task<ItemListDTO> GetByCityAsync(string city);
         Task<bool> DeleteAsync(int id);
         Task<IEnumerable<ItemListDTO>> GetAllAsync();
-        Task<IEnumerable<ItemListDTO>> GetAllByUserIdAsync(int userid);
-        Task<ItemListDTO> GetByIdAsync(int id);
-        Task<ItemList> CopyAsync(int itemListid, int userid);
+        Task<IEnumerable<ItemListDTO>> GetAllByUserIdAsync(int userId);
+        Task<IEnumerable<ItemListDTO>> GetByCityAsync(string city);
+        Task<ItemListDTO> GetByIdAsync(int itemListId);
+        Task<IEnumerable<ItemListDTO>> GetByMonthAndCity(DateTime date, string city);
         Task<bool> UpdateAsync(UpdateItemListDTO dto, int id);
     }
 }
