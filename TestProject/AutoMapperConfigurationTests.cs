60c8b96dcf6f76fedad0cd052f6948be5a90b68f
Bartosz Bialokozowicz <b.bialokozowicz@gmail.com>
2024-02-06 23:12:26 +0100

    AuthServiceTests, AutoMapperConfigurationTests, UserControllerTest added

---
+++ TestProject/AutoMapperConfigurationTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CheckedAppProject.LOGIC.AutoMapperProfiles;
+
+[TestFixture]
+public class AutoMapperConfigurationTests
+{
+    [Test]
+    public void AutoMapper_Configuration_IsValid()
+    {
+        var mapperConfiguration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new ItemListMappingProfile());
+            cfg.AddProfile(new UserMappingProfile());
+        });
+
+        mapperConfiguration.AssertConfigurationIsValid();
+    }
+}
