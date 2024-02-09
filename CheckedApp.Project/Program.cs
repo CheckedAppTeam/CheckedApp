06659aa3d34fa4e0d039a0424fc4241724ac726d
Bartosz Bialokozowicz <b.bialokozowicz@gmail.com>
2024-02-09 09:14:36 +0100

    Program refactor

--- CheckedApp.Project/Program.cs
+++ CheckedApp.Project/Program.cs
@@ -6,7 +6,6 @@ LoggingConfiguration.ConfigureLogging(builder);
 StartupConfiguration.ConfigureServices(builder);
 
 var app = builder.Build();
-// New Comment to delete
 
 StartupConfiguration.ConfigureApp(app);
 
